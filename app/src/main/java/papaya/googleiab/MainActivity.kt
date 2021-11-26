2465079d69b0cd1602d2ebe16542778a1e8c857a
Shubham Pandey <shubhampandey979271@gmail.com>
2021-11-26 14:46:37 +0530

    Initial commit

---
+++ app/src/main/java/papaya/googleiab/MainActivity.kt
@@ -0,0 +1,62 @@
+package papaya.googleiab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.android.billingclient.api.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.buyNow)
+
+        val skuList = ArrayList<String>()
+        skuList.add("android.test.purchased")
+
+        val purchasesUpdatedListener = PurchasesUpdatedListener{
+            billingResult, purchses ->
+        }
+
+        var billingClient = BillingClient.newBuilder(this)
+            .setListener(purchasesUpdatedListener)
+            .enablePendingPurchases().build()
+
+        button.setOnClickListener {
+
+            billingClient.startConnection(object : BillingClientStateListener{
+                override fun onBillingServiceDisconnected() {
+//                    TODO("Not yet implemented")
+                }
+
+                override fun onBillingSetupFinished(billingResult: BillingResult) {
+//                    TODO("Not yet implemented")
+
+                    if (billingResult.responseCode == BillingClient.BillingResponseCode.OK){
+
+                        val params = SkuDetailsParams.newBuilder()
+                        params.setSkusList(skuList)
+                            .setType(BillingClient.SkuType.INAPP)
+
+                        billingClient.querySkuDetailsAsync(params.build()){
+                            billingResult, skuDetailsList ->
+
+                            for (skuDetails in skuDetailsList!!) {
+                                val flowPurchase = BillingFlowParams.newBuilder()
+                                    .setSkuDetails(skuDetails)
+                                    .build()
+
+                                val responseCode = billingClient.launchBillingFlow(this@MainActivity, flowPurchase).responseCode
+                            }
+                        }
+                    }
+                }
+
+            })
+        }
+    }
+}
